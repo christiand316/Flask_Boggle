dcda2f885643b7f867c38ddc043d4f4cb59f2096
christiand316 <cjd2020hb@gmail.com>
2023-06-23 10:15:18 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from boggle import Boggle
+
+boggle_game = Boggle()
+
+
+
+@app.route('/')
+def home():
+     
+    board = 
+
+    return render_template('base.html')
+
+@app.route('/valid-check')
+
+
+
+@app.route('/score')
+def score():
+    
+
